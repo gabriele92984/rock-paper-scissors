65514bb57b3af1d095a1b895266f1971c11e8fb8
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2024-06-27 18:02:53 +0200

    Correct indentation

--- javascript.js
+++ javascript.js
@@ -1,16 +1,15 @@
 // Write the logic to get the computer choice.
 
   // Create a new function named "getComputerChoice".
-  /*
-  function getComputerChoice(min, max) {
-    const minCeiled = Math.ceil(min);
-    const maxFloored = Math.floor(max);
-    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
-  }
+
+  /*  function getComputerChoice(min, max) {
+        const minCeiled = Math.ceil(min);
+        const maxFloored = Math.floor(max);
+        return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+      }
   
-  let computerChoice = getComputerChoice(1, 3);
-  console.log(computerChoice)
-  */
+      let computerChoice = getComputerChoice(1, 3);
+      console.log(computerChoice) */
 
   function getComputerChoice(max) {
     return Math.floor(Math.random() * max);
