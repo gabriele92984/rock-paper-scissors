32f8dac49ce14451d8b707f9b02be2bc5de8f61a
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2024-06-27 17:58:15 +0200

    Rewrite getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 // Write the logic to get the computer choice.
 
   // Create a new function named "getComputerChoice".
+  /*
   function getComputerChoice(min, max) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
@@ -9,7 +10,15 @@
   
   let computerChoice = getComputerChoice(1, 3);
   console.log(computerChoice)
+  */
+
+  function getComputerChoice(max) {
+    return Math.floor(Math.random() * max);
+  }
   
+  let computerChoice = getComputerChoice (3);
+  console.log(computerChoice)
+
   // Write the code so that "getComputerChoice" will ramdomly "return" one of the string values: "rock", "paper", "scissors".
   
 
