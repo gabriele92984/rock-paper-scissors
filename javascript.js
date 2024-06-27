d1bc92cf902356d7d76368f426842754e06fc311
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2024-06-27 17:48:51 +0200

    Add computerChoice variable

--- javascript.js
+++ javascript.js
@@ -1,15 +1,17 @@
 // Write the logic to get the computer choice.
 
   // Create a new function named "getComputerChoice".
-
   function getComputerChoice(min, max) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
   }
-  console.log(getComputerChoice(1, 3));
+  
+  let computerChoice = getComputerChoice(1, 3);
+  console.log(computerChoice)
+  
+  // Write the code so that "getComputerChoice" will ramdomly "return" one of the string values: "rock", "paper", "scissors".
   
 
-  // Write the code so that "getComputerChoice" will ramdomly "return" one of the string values: "rock", "paper", "scissors".
   
   // Test that the function returns what expected.
