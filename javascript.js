fe2b8a6f62656a25b4e5f0e7bb84f7cdc1e0eb9c
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2024-06-27 19:18:14 +0200

    Review logic code to get computer choice

--- javascript.js
+++ javascript.js
@@ -5,17 +5,19 @@
     return Math.floor(Math.random() * max);
   }
 
-  let computerChoice = getComputerChoice(3).toString();
+  let computerChoice = getComputerChoice(3);
+  
   console.log(computerChoice)
 
   // Write the code so that "getComputerChoice" will ramdomly "return" one of the string values: "rock", "paper", "scissors".
-  if (computerChoice === "0") {
+  if (computerChoice === 0) {
     computerChoice ="rock";
-  } else if (computerChoice === "1") {
+  } else if (computerChoice === 1) {
     computerChoice = "paper";
   } else {
     computerChoice = "scissors"
   }
+
   console.log(computerChoice)
 
   // Test that the function returns what expected.
