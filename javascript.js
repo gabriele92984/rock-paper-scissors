6834d57374dd011e79c2cef6e56dbd18e86eccb5
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2024-06-29 10:56:08 +0200

    Add computer logic funcion with array

--- javascript.js
+++ javascript.js
@@ -1,7 +1,20 @@
 // Write the logic to get the computer choice.
   // Create a new function named "getComputerChoice".
   // Write the code so that "getComputerChoice" will ramdomly "return" one of the string values: "rock", "paper", "scissors".
-  function getRandomInt(max) {
+  
+  // Computer logic function with array
+  
+  function getComputerChoice() { 
+  let choices = ["rock", "paper", "scissors"];
+  let randomChoice = Math.floor((Math.random() * choices.length));
+  return choices[randomChoice];
+}
+
+// Test the function
+  console.log(getComputerChoice());  
+
+
+  /*function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
   
@@ -19,33 +32,34 @@
   }
 
   // Test that the function returns what expected.
-  getComputerChoice();
-  console.log(getComputerChoice());
+    console.log(getComputerChoice());*/
 
 
 // Write the logic to get the human choice
   // Create a new function named "getHumanChoice".
   // Write the code so that "getHumanChoice" will return one of the valid choices depending on what the user inputs.
-  function getHumanChoice() {
-    let choice;
-    choice = prompt("Welcome to rock-paper-scissors game! Please make your choice:");
+  
+  /*function getHumanChoice() {
+    let choice = prompt("Choose rock, paper or scissors:");
+    choice = choice.toLocaleLowerCase();
 
-    if (choice.toLowerCase() === "rock") {
+    while (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
+      choice = prompt("Invalid choice! Please choose 'rock', 'paper', or 'scissors'");
+    }
+    return choice;
+  }*/
+
+  /* if (choice.toLowerCase() === "rock") {
       return choice;
     } else if (choice.toLowerCase() === "paper") {
       return choice;
     } else if (choice.toLowerCase() === "scissors") {
       return choice;
     } else {
-      return choice = prompt (`Please enter a valid entry, choose one of the following:
-      "rock"
-      "paper"
-      "scissors"
-      `);
-    }
-  }
-
+      alert ("Please enter a valid entry, choose one of the following:\n'Rock'\n'Paper'\n'Scissors'");
+    }*/
 
   // Test what your function returns by using "console.log".
-  console.log(getHumanChoice());
+  //console.log(getHumanChoice());
+  
   
