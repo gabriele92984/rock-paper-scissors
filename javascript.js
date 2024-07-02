58e321c76e37c6604aa85b3f545aa60c9b4d2f2e
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2024-07-02 08:17:18 +0200

    Clean up comments

--- javascript.js
+++ javascript.js
@@ -1,34 +1,11 @@
 // Write the logic to get the computer choice.
-  // Create a new function named "getComputerChoice".
-  // Write the code so that "getComputerChoice" will ramdomly "return" one of the string values: "rock", "paper", "scissors".
-  
   function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"];
     let randomChoice = Math.floor(Math.random() * choices.length);
     return choices[randomChoice];
 }
-  
-  /*function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
-  
-  let result = getRandomInt(3);
-  console.log(result);
-
-  function getComputerChoice() {
-    if (result === 0) {
-      return "rock";
-    } else if (result === 1) {
-      return "paper";
-    } else {
-      return "scissors";
-    }
-  }*/
 
 // Write the logic to get the human choice.
-  // Create a new function named "getHumanChoice".
-  // Write the code so that "getHumanChoice" will return one of the valid choices depending on what the user inputs.
-  
   function getHumanChoice() {
     let choice = prompt("Choose rock, paper or scissors:");
     choice = choice.toLowerCase();
@@ -40,17 +17,13 @@
     return choice;
   }
 
+
 // Declare the players score variables.
   let humanScore = 0;
   let computerScore = 0;
-
-// Write the logic to play a single round.
-  // Your game will be played round by round. You will write a function that takes the human and computer player choices as arguments, plays a single round, increments the round winner’s score and logs a winner announcement.
   
 
 // Write the logic to play the entire game.
-  // Your game will play 5 rounds. You will write a function named playGame that calls playRound to play 5 rounds, keeps track of the scores and declares a winner at the end.
-  
   function playGame(n) {
 
     if (n > 0) {
