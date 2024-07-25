d07532d080ddc682b24bfc06d4668d5c800d98d0
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2024-07-25 07:54:54 +0200

    Add a div for displaying results

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let humanScore = 0;
 let computerScore = 0;
 
-const rockButton = document.querySelector('.rock');
-const paperButton = document.querySelector('.paper');
-const scissorsButton = document.querySelector('.scissors');
+const rockButton = document.querySelector(".rock");
+const paperButton = document.querySelector(".paper");
+const scissorsButton = document.querySelector(".scissors");
+const outcomeDiv = document.querySelector(".outcome");
 
 const computerChoice = () => {
   let choices = ["rock", "paper", "scissors"];
@@ -12,49 +13,57 @@ const computerChoice = () => {
 }
 
 const playRound = (humanSelection, computerSelection) => {
+  console.log(humanSelection, computerSelection);
   if (humanSelection === computerSelection) {
     const p = document.createElement("p");
     p.innerText = `You tied! You both picked ${humanSelection}`;
+    outcomeDiv.appendChild(p);
   } else if (humanSelection === "scissors" && computerSelection === "rock") {
     computerScore++;
     const p = document.createElement("p");
     p.innerText = "You lost! Rock crushes scissors";
+    outcomeDiv.appendChild(p);
   } else if (humanSelection === "scissors" && computerSelection === "paper") {
     humanScore++;
     const p = document.createElement("p");
     p.innerText = "You won! Scissors cuts paper";
+    outcomeDiv.appendChild(p);
   } else if (humanSelection === "rock" && computerSelection === "paper") {
     computerScore++;
     const p = document.createElement("p");
     p.innerText = "You lost! Paper covers rock";
+    outcomeDiv.appendChild(p);
   } else if (humanSelection === "rock" && computerSelection === "scissors") {
     humanScore++;
     const p = document.createElement("p");
     p.innerText = "You won! Rock crushes scissors";
+    outcomeDiv.appendChild(p);
   } else if (humanSelection === "paper" && computerSelection === "scissors") {
     computerScore++;
     const p = document.createElement("p");
     p.innerText = "You lost! Scissors cuts paper";
+    outcomeDiv.appendChild(p);
   } else if (humanSelection === "paper" && computerSelection === "rock") {
     humanScore++;
     const p = document.createElement("p");
     p.innerText = "You won! Paper covers rock";
+    outcomeDiv.appendChild(p);
   }
 }
 
-rockButton.addEventListener("clic", () =>{
+rockButton.addEventListener("click", () =>{
   const computerSelection = computerChoice();
   const humanSelection = "rock";
   playRound(humanSelection, computerSelection);
 })
 
-paperButton.addEventListener("clic", () =>{
+paperButton.addEventListener("click", () =>{
   const computerSelection = computerChoice();
   const humanSelection = "paper";
   playRound(humanSelection, computerSelection);
 })
 
-scissorsButton.addEventListener("clic", () =>{
+scissorsButton.addEventListener("click", () =>{
   const computerSelection = computerChoice();
   const humanSelection = "scissors";
   playRound(humanSelection, computerSelection);
